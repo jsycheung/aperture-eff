46ef7127734a86c3bce5d828a19843822fe0177e
Jasmine Cheung <jasminesyc2000@gmail.com>
2023-08-10 03:09:49 -0400

    added basic functionalities

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, redirect, url_for, flash
+from forms import CalculationForm
+from lists import coeff_dict
 
 # Create a Flask application object.
 app = Flask(__name__)
 # Define app secret key.
 app.secret_key = "keep this secret"
+
+# make a page that returns page not found
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    '''Render homepage for calculations.'''
+    cal_form = CalculationForm()
+    if cal_form.validate_on_submit():
+        pass
+    return render_template("calculation.html", cal_form=cal_form, coeff_dict=coeff_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, DecimalField
-from wtforms.validators import NumberRange
+from wtforms import SubmitField, DecimalField, SelectField
+from wtforms.validators import NumberRange, InputRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from lists import src_list
 
 
 class CalculationForm(FlaskForm):
-    obsfreq = DecimalField("Observation Frequency", validators=[
-        NumberRange(min=0)])
-    submit = SubmitField("Submit")
+    file = FileField("Text file: ", validators=[FileRequired(), FileAllowed(
+        ['txt'], "Please upload file in .txt format!")])
+    src_name = SelectField("Source name: ", coerce=int,
+                           choices=src_list, validators=[InputRequired()])
+    diameter = DecimalField("Diameter of aperture (m): ", validators=[
+                            InputRequired(), NumberRange(min=0)])
+    submit = SubmitField("Compute")
