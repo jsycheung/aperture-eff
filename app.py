6170e249d947f80a4df634d6b418b0ab8e1d8a6d
Jasmine Cheung <jasminesyc2000@gmail.com>
2023-08-10 19:31:50 -0400

    improved user interface

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from forms import CalculationForm
-from lists import coeff_dict
+from lists import coeff_dict, src_list
 import math
 
 # Create a Flask application object.
@@ -20,16 +20,25 @@ def page_not_found(e):
 def home():
     '''Render homepage for calculations.'''
     cal_form = CalculationForm()
+    return render_template("calculation.html", cal_form=cal_form, coeff_dict=coeff_dict)
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    cal_form = CalculationForm()
     if cal_form.validate_on_submit():
-        src = cal_form.src_name.data
+        src_idx = cal_form.src_name.data
+        src = src_list[src_idx][1]
         diameter = cal_form.diameter.data
         obs_freq = 1.395
         s_v = coeff_dict[src]["a0"]
         t_a = 1
         eta_a = 1
-        a_eff = eta_a * math.pi / 4 * (diameter**2)
-        return render_template("result.html", src=src, diameter=diameter, obs_freq=obs_freq, s_v=s_v, t_a=t_a, eta_a=eta_a, a_eff=a_eff)
-    return render_template("calculation.html", cal_form=cal_form, coeff_dict=coeff_dict)
+        a_eff = eta_a * math.pi / 4 * (float(diameter)**2)
+        print("successful")
+        flash("Calculation successful!", "success")
+        return render_template("result.html", coeff_dict=coeff_dict, src=src, diameter=diameter, obs_freq=obs_freq, s_v=s_v, t_a=t_a, eta_a=eta_a, a_eff=a_eff)
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -8,8 +8,8 @@ from lists import src_list
 class CalculationForm(FlaskForm):
     file = FileField("Text file: ", validators=[FileRequired(), FileAllowed(
         ['txt'], "Please upload file in .txt format!")])
-    src_name = SelectField("Source name: ", coerce=int,
-                           choices=src_list, validators=[InputRequired()])
-    diameter = DecimalField("Diameter of aperture (m): ", validators=[
+    src_name = SelectField("Source name", choices=src_list,
+                           coerce=int, validators=[InputRequired()])
+    diameter = DecimalField("Diameter of aperture (m)", validators=[
                             InputRequired(), NumberRange(min=0)])
     submit = SubmitField("Compute")
