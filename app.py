7eb4ff7505ab0ebc39e9bf286c95cb16a9d6fb21
Jasmine Cheung <jasminesyc2000@gmail.com>
2023-08-10 22:54:55 -0400

    web app completed, not yet deployed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, flash, request
 from forms import CalculationForm
 from lists import coeff_dict, src_list
+from calc_values import calc_Ta_obs
 import math
 
 # Create a Flask application object.
@@ -27,17 +28,18 @@ def home():
 def result():
     cal_form = CalculationForm()
     if cal_form.validate_on_submit():
+        file = request.files['file']
+        t_a_prime, t_a_std, obs_freq = calc_Ta_obs(file)
         src_idx = cal_form.src_name.data
         src = src_list[src_idx][1]
         diameter = cal_form.diameter.data
-        obs_freq = 1.395
-        s_v = coeff_dict[src]["a0"]
-        t_a = 1
-        eta_a = 1
+        s_v = coeff_dict[src]["a0"] + coeff_dict[src]["a1"]*math.log10(obs_freq) + coeff_dict[src]["a2"]*(math.log10(obs_freq))**2 + coeff_dict[src]["a3"]*(
+            math.log10(obs_freq))**3 + coeff_dict[src]["a4"]*(math.log10(obs_freq))**4 + coeff_dict[src]["a5"]*(math.log10(obs_freq))**5
+        eta_a = 3520/(float(diameter)**2)*t_a_prime/s_v
         a_eff = eta_a * math.pi / 4 * (float(diameter)**2)
-        print("successful")
         flash("Calculation successful!", "success")
-        return render_template("result.html", coeff_dict=coeff_dict, src=src, diameter=diameter, obs_freq=obs_freq, s_v=s_v, t_a=t_a, eta_a=eta_a, a_eff=a_eff)
+        return render_template("result.html", coeff_dict=coeff_dict, src=src, diameter=diameter, obs_freq=obs_freq, s_v=s_v, t_a_prime=t_a_prime, t_a_std=t_a_std, eta_a=eta_a, a_eff=a_eff)
+    print(cal_form.errors)
     return redirect(url_for('home'))
 
 
--- calc_values.py
+++ calc_values.py
@@ -1,70 +1,74 @@
 import os
 import numpy as np
 
-def calc_Ta(filename):
-    #reads data and calculates aperture temperature
-    #input: calibrated data file from 20m lowres on/off scan of a point source
-    #output: mean Ta, std Ta, observation center frequency
-    #NOTE: Only reads power data from XX column (because YY data was bad)
-    
-    #_power arrays sum recorded powers
+
+def calc_Ta_obs(filename):
+    # reads data and calculates aperture temperature
+    # input: calibrated data file from 20m lowres on/off scan of a point source
+    # output: mean Ta, std Ta, observation center frequency
+    # NOTE: Only reads power data from XX column (because YY data was bad)
+
+    # _power arrays sum recorded powers
     on_power = []
     off_power = []
-    #_count arrays count number of data points for each scan (so we can calculate an average)
+    # _count arrays count number of data points for each scan (so we can calculate an average)
     on_count = []
     off_count = []
 
-    with open(filename) as f:
-        reading_data = False
-        for line in f:
-            if "OBSFREQ" in line:
-                center_freq = float(line.split("=")[-1]) / 1000
-            if "ELEV(deg)" in line:
-                elevation = float(line.split("=")[-1])
-            #all parameter lines at top of file start with #
-            if line[0] == "#":
+    # with open(filename) as f:
+    f = filename.readlines()
+    reading_data = False
+    for line in f:
+        line = line.decode()
+        if "OBSFREQ" in line:
+            center_freq = float(line.split("=")[-1]) / 1000
+        if "ELEV(deg)" in line:
+            elevation = float(line.split("=")[-1])
+        # all parameter lines at top of file start with #
+        if line[0] == "#":
+            continue
+        else:
+            row = line.split()
+            # if row[9] isn't 1, it is a bad data point (either calibration or sweep)
+            if row[9] != '1':
                 continue
+            cycle = int(int(row[8]) / 2)
+            power = float(row[6])
+            if row[10] == "on":
+                on = True
             else:
-                row = line.split()
-                #if row[9] isn't 1, it is a bad data point (either calibration or sweep)
-                if row[9] != '1':
-                    continue
-                cycle = int(int(row[8]) / 2)
-                power = float(row[6])
-                if row[10] == "on":
-                    on = True
+                on = False
+
+            if on:
+                if len(on_power) < cycle+1:
+                    on_power.append(power)
+                    on_count.append(1)
                 else:
-                    on = False
-
-                if on:
-                    if len(on_power) < cycle+1:
-                        on_power.append(power)
-                        on_count.append(1)
-                    else:
-                        on_power[cycle] += power
-                        on_count[cycle] += 1
+                    on_power[cycle] += power
+                    on_count[cycle] += 1
+            else:
+                if len(off_power) < cycle+1:
+                    off_power.append(power)
+                    off_count.append(1)
                 else:
-                    if len(off_power) < cycle+1:
-                        off_power.append(power)
-                        off_count.append(1)
-                    else:
-                        off_power[cycle] += power
-                        off_count[cycle] += 1
+                    off_power[cycle] += power
+                    off_count[cycle] += 1
 
-    on_power_avg = np.divide(on_power,on_count)
-    off_power_avg = np.divide(off_power,off_count)
-    aperture_temps = np.subtract(on_power_avg,off_power_avg)
-    A = 1/(np.sin(elevation*180/np.pi)) #approximation valid for elev > 15 deg
-    correction = np.exp(A*0.01) #assumes tau = 0.01
-    corrected_aperture_temps = aperture_temps * correction
-    
-    return np.mean(corrected_aperture_temps), np.std(corrected_aperture_temps), center_freq
+    on_power_avg = np.divide(on_power, on_count)
+    off_power_avg = np.divide(off_power, off_count)
+    antenna_temps = np.subtract(on_power_avg, off_power_avg)
+    # approximation valid for elev > 15 deg
+    A = 1/(np.sin(elevation*np.pi/180))
+    correction = np.exp(A*0.01)  # assumes tau = 0.01
+    corrected_antenna_temps = antenna_temps * correction
+
+    return np.mean(corrected_antenna_temps), np.std(corrected_antenna_temps), center_freq
 
 
-def calc_S(center_freq,coeffs):
-    #calculate flux
-    #assumes flux can be calculated with a polynomial as in:
-    #https://iopscience.iop.org/article/10.3847/1538-4365/aa6df9/pdf
+def calc_S(center_freq, coeffs):
+    # calculate flux
+    # assumes flux can be calculated with a polynomial as in:
+    # https://iopscience.iop.org/article/10.3847/1538-4365/aa6df9/pdf
     logS = 0
     logV = np.log10(center_freq)
     for i, a in enumerate(coeffs):
